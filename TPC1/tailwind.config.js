e647a2fb9e1eb07f09eae076fbb253c6112bdbe7
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-02-21 11:48:43 +0000

    Website structure and navbar.

---
+++ TPC1/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  plugins: [],
+
+  theme: {
+
+    extend: {
+
+      colors: {
+        text: '#404040'
+      },
+
+    }
+  }
+}
