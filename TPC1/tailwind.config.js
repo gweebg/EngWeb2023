c8931fffeb521e70ae9ab1d442c8f5b1137c21b6
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-02-21 18:21:44 +0000

    Landing page and intro.

--- TPC1/tailwind.config.js
+++ TPC1/tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
     extend: {
 
       colors: {
-        text: '#404040'
+        text: '#404040',
+        green_primary: '#2a8636',
+        green_secondary: '#39B54A'
       },
 
     }
