8c111660efeda0dee2d42b7d4482b5fe7ae9264e
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-03-04 16:14:06 +0000

    Half done.

---
+++ TPC3/src/pages/people.js
@@ -0,0 +1,63 @@
+
+exports.servePeople = (personList) => {
+
+    let template = `
+        <!DOCTYPE html>
+        <html lang="en">
+        
+            <head>
+                <meta charset="UTF-8">
+                <link rel="stylesheet" href="styles.css">
+                <title>Template list users.</title>
+            </head>
+        
+            <script>
+                function filterAndDisplay() {
+        
+                    let input = document.getElementById('input');
+                    let filter = input.value.toUpperCase();
+        
+                    let list = document.getElementById('list');
+                    let items = list.getElementsByTagName('a');
+        
+                    for (let i = 0; i < items.length; i++) {
+        
+                        let a = items[i];
+                        let value = a.textContent || a.innerText;
+        
+                        if (value.toUpperCase().indexOf(filter) > -1) items[i].style.display = "";
+                        else items[i].style.display = "none";
+                    }
+                }
+            </script>
+        
+            <body>
+        
+                <div class="centered">
+        
+                    <div class="list">
+        
+                        <input type="text" id="input" onkeyup="filterAndDisplay()" placeholder="Pesquisar pessoas pelo nome..">
+        
+                        <div class="grid" id="list">
+    `;
+
+    personList.forEach(person => {
+
+        template += `
+            <a href="/people/${person.id}" class="grid-item">
+                ${person.nome}
+            </a>
+        `;
+    });
+
+    template += `
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+
+    return template;
+};
---
+++ TPC3/src/pages/person.js
@@ -0,0 +1,100 @@
+
+function titleCase(str) {
+    return str.toLowerCase().split(' ').map(function(word) {
+        return word.replace(word[0], word[0].toUpperCase());
+    }).join(' ');
+}
+
+exports.servePerson = (person) => {
+
+    let template = `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <title>${person.nome}</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    
+    
+    <body>
+    
+    <div class="centered">
+    
+        <div class="details">
+    
+            <h3>(${person.BI}) ${person.nome}</h3>
+        
+            <p class="text">
+                <strong>Age: </strong> ${person.idade}
+            </p>
+    
+            <p class="text">
+                <strong>Gender: </strong> ${person.sexo}
+            </p>
+    
+            <p class="text">
+                <strong>Address: </strong> ${person.morada.cidade}, ${person.morada.distrito}
+            </p>
+    
+            <p class="text">
+                <strong>Job: </strong> ${person.profissao}
+            </p>
+    
+            <p class="text">
+                <strong>Political Party: </strong> ${person.partido_politico.party_name} (${person.partido_politico.abbr})
+            </p>
+    
+            <p class="text">
+                <strong>Religion: </strong> ${person.religiao}
+            </p>
+    
+            <p class="text">
+                <strong>Sports: </strong> ${person.desportos.join(', ')}
+            </p>
+    
+            <p class="text">
+                <strong>Animals/Pets: </strong> ${person.animais.join(', ')}
+            </p>
+    
+            <p class="text">
+                <strong>Celebrities (PT): </strong> ${person.figura_publica_pt.join(', ')}
+            </p>
+    
+            <p class="text">
+                <strong>Car Brand: </strong> ${person.marca_carro}
+            </p>
+    
+            <p class="text">
+                <strong>Favourite Destinations </strong> ${person.destinos_favoritos.join(', ')}
+            </p>
+    
+            <div class="connections">
+    
+                <h3>Attributes</h3>
+                
+                <div class="grid-con">
+    `;
+
+
+    for (let attr in person.atributos) {
+        if (person.atributos[attr]) template += `<p class="grid-item">${titleCase(attr.replace('_', ' '))}</p>`;
+    }
+
+    template += `
+                            </div>
+                        <script>
+                            document.write('<h3><a style="text-decoration: none; color: #404040" href="' + document.referrer + '">Go Back</a></h3>');
+                        </script>
+                    </div>   
+                </div>   
+            </div>
+        </body>
+    </html>
+    `;
+
+    return template;
+
+};
